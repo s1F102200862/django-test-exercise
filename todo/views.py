83d8c00d25810411023a4df2a2773dde449219e4
1F10220086 <s1f102200862@iniad.org>
2023-07-06 13:59:08 +0900

    修正４回目

--- todo/views.py
+++ todo/views.py
@@ -12,7 +12,7 @@ def index(request):
     tasks = Task.objects.all()
 
     if request.GET.get('order') == 'due':
-          tasks = Task.objects.order_by('due_at')
+        tasks = Task.objects.order_by('due_at')
     else:
         tasks = Task.objects.order_by('-posted_at')
     context = {
